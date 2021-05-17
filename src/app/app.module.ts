a6125de4a7d732d25ff0ab24992488f616c86e96
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-05-17 18:54:16 +0530

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,20 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { DemoMaterialModule } from './material-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import { RouterModule, Routes } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactformService } from './contact-form/contact.service';
+import { UsersComponent } from './users/users.component';
 
+const appRoutes: Routes = [
+    { path: 'contact-form',component: ContactFormComponent},  
+    { path: 'users', component: UsersComponent}
+];
 @NgModule({
   imports:      [ 
     BrowserModule,
@@ -22,11 +29,13 @@ import { ContactformService } from './contact-form/contact.service';
     DemoMaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
+     RouterModule.forRoot(appRoutes),
      ],
-
   entryComponents: [],
-  declarations: [ AppComponent, HelloComponent, ContactFormComponent,  ],
+  declarations: [ AppComponent, HelloComponent, ContactFormComponent, UsersComponent,  ],
   bootstrap:    [ AppComponent ],
-   providers: [ ContactformService ]
+   providers: [ ContactformService ],
+   exports: [RouterModule],
 })
+
 export class AppModule { }
